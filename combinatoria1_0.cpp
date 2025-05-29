a3c5d73180e5b9fcf255b78d89b2c71740aaec46
JoseLuisChambiCopari <jchambic@unjbg.edu.pe>
2025-05-29 00:23:20 -0500

    Segunda version combinatoria

--- combinatoria1_0.cpp
+++ combinatoria1_0.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 using namespace std;
-int factorial (int);
+int factorial(int);
 int main() {
-	int m,n,c;
+	int m, n, c;
 	cout<<"Ingrese m: ";
 	cin>>m;
 	cout<<"Ingrese n: ";
 	cin>>n;
-	c=factorial(m) / (factorial(n) * factorial (m-n));
-	cout<<c;
+	if (n>m) {
+		cout<<"n no puede ser mayor que m.";
+	} else{
+		c = factorial(m) / (factorial(n) * factorial(m - n));
+		cout<<c;
+	}
 	return 0;
 }
-int factorial (int n) {
-	int f;
-	f=1;
-	for(int i=2;i<=n;i++) {
-		f=f*i;
+
+int factorial(int n) {
+	int f=1;
+	for (int i=2;i<=n;i++) {
+		f = f*i;
 	}
 	return f;
 }
