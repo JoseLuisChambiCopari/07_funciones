6e2070919e82c6b7ffada1932d10698447a2a697
JoseLuisChambiCopari <jchambic@unjbg.edu.pe>
2025-05-29 19:38:18 -0500

    Agrega ejercicio 2: primo o no

---
+++ ejercicio_2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int verificar (int);
+int main() {
+	int n;
+	cout<<"Ingrese un numero para la verificacion: ";
+	cin>>n;
+	if (verificar(n)==1) {
+		cout<<"El numero es primo";
+	} else {
+		cout<<"El numero no es primo";
+	}
+	return 0;
+}
+
+int verificar(int n) {
+	int i,divis;
+	divis=0;
+	for(i=1; i<=n; i++) {
+        if (n%i==0) {
+            divis=divis+1;
+        }
+    }
+    if (divis==2){
+    	return 1;
+	}
+	return 0;
+}
