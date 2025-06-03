6a49ebe05158fe76fc4e9601455be375b4803b65
JoseLuisChambiCopari <jchambic@unjbg.edu.pe>
2025-06-03 18:13:41 -0500

    Actualiza ejercicio 7: Se agrega la accion de volver a intentar en caso de que el usuario ingrese un numero negativo

--- ejercicio_7.cpp
+++ ejercicio_7.cpp
@@ -4,10 +4,16 @@ void calcularCocienteyResto (int, int);
 int main(){
     int a, b;
     cout <<"INGRESE LOS NUMEROS"<<endl;
-    cout<<"Primer numero: ";
-    cin>>a;
-    cout<<"Segundo numero: ";
-    cin>>b;
+    do {
+        cout<<"Primer numero: ";
+        cin>>a;
+        cout<<"Segundo numero: ";
+        cin>>b;
+        if (a<=0 || b<=0) {
+            cout<<"Los numeros deben ser positivo para el calculo."<<endl;
+            cout<<"---------------------------------------------"<<endl;
+        }
+    } while (a<=0 || b<=0);
     calcularCocienteyResto (a, b);
     return 0;
 }
