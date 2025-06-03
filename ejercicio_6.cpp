a0d143d1094a090d8b6c5c6f8cf26ae0ca3efc8e
JoseLuisChambiCopari <jchambic@unjbg.edu.pe>
2025-06-02 21:28:33 -0500

    Agrega ejercicio 6: Sumatoria de la serie Fibonacci

---
+++ ejercicio_6.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+void mostrarFibonacci (int);
+int sumaFibonacci (int);
+int main() {
+	int n,suma;
+	cout<<"Ingrese el numero de terminos que quiere que tenga la serie Fibonacci: ";
+	cin>>n;
+	cout<<"La suma de la serie: "<<endl;
+	mostrarFibonacci(n);
+	suma = sumaFibonacci(n);
+	cout<<"\nes:"<<suma;
+	return 0;
+}
+
+void mostrarFibonacci(int n) {
+	int a=0,b=1,resultado=0;
+	for (int i=0;i<n;i++) {
+		cout<<resultado<<", ";
+		a=b;
+		b=resultado;
+		resultado=a+b;
+	}
+}
+
+int sumaFibonacci(int n) {
+	int a,b,suma,resultado;
+	a=0;
+	b=1;
+	resultado=0;
+	suma=0;
+	for (int i=0;i<n;i++) {
+		suma=suma+resultado;
+		a=b;
+		b=resultado;
+		resultado=a+b;
+	}
+	return suma;
+}
